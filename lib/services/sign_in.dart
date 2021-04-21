9c62655998892e5acec4c0d814e69e6382625f19
Wallynson Rodrigo <aplicativoswr@gmail.com>
2021-04-21 11:05:47 -0400

    Alterações para login no iOS

--- lib/services/sign_in.dart
+++ lib/services/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -14,54 +15,65 @@ String imageUrl;
 String usuario;
 
 Future<String> signInWithGoogle() async {
-  await Firebase.initializeApp();
+  try {
+    await Firebase.initializeApp();
 
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount
+        .authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.credential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
 
-  final UserCredential authResult = await _auth.signInWithCredential(credential);
-  final User user = authResult.user;
+    final UserCredential authResult = await _auth.signInWithCredential(
+        credential);
+    final User user = authResult.user;
 
-  if (user != null) {
-    // Add the following lines after getting the user
-    // Checking if email and name is null
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(user.photoURL != null);
+    if (user != null) {
+      // Add the following lines after getting the user
+      // Checking if email and name is null
+      assert(user.email != null);
+      assert(user.displayName != null);
+      assert(user.photoURL != null);
 
-    // Store the retrieved data
-    name = user.displayName;
-    email = user.email;
-    imageUrl = user.photoURL;
+      // Store the retrieved data
+      name = user.displayName;
+      email = user.email;
+      imageUrl = user.photoURL;
 
-    if (email.contains("@")) {
-      usuario = email.substring(0, email.indexOf("@"));
-      if(usuario.contains('.') || usuario.contains('#') || usuario.contains('\$') ||
-          usuario.contains('[') || usuario.contains(']')) {
-        usuario = usuario.replaceAll('\.', '');
-        usuario = usuario.replaceAll('#', '');
-        usuario = usuario.replaceAll('\$', '');
-        usuario = usuario.replaceAll('[', '');
-        usuario = usuario.replaceAll(']', '');
+      if (email.contains("@")) {
+        usuario = email.substring(0, email.indexOf("@"));
+        if (usuario.contains('.') || usuario.contains('#') ||
+            usuario.contains('\$') ||
+            usuario.contains('[') || usuario.contains(']')) {
+          usuario = usuario.replaceAll('\.', '');
+          usuario = usuario.replaceAll('#', '');
+          usuario = usuario.replaceAll('\$', '');
+          usuario = usuario.replaceAll('[', '');
+          usuario = usuario.replaceAll(']', '');
+        }
       }
-    }
 
-    // Only taking the first part of the name, i.e., First Name
-    /*if (name.contains(" ")) {
+      // Only taking the first part of the name, i.e., First Name
+      /*if (name.contains(" ")) {
       name = name.substring(0, name.indexOf(" "));
     }*/
-    CircularProgressIndicator();
+      CircularProgressIndicator();
 
-    print('$email acessou o sistema');
-    return '$user';
-  }
+      print('$email acessou o sistema');
+      return '$user';
+    }
 
     return null;
+  } catch (NSInvalidArgumentException) {
+    Fluttertoast.showToast(
+      msg:'Houve um erro.',
+      toastLength: Toast.LENGTH_SHORT,
+      timeInSecForIosWeb: 2,
+    );
+  }
 }
 
 Future<void> signOutGoogle() async {
